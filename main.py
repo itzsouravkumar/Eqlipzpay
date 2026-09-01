e353199270740e3f529d2095780ee7bef3fc5b7f
itzsouravkumar <oyesourav05012006@gmail.com>
2026-09-01 23:11:00 +0530

    feat: add favicon to landing page, dashboard, and API documentation

--- main.py
+++ main.py
@@ -668,6 +668,7 @@ async def custom_swagger_ui_html(req: Request):
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
         swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.9.0/swagger-ui.css",
+        swagger_favicon_url="/static_front/logo.png",
     ).body.decode("utf-8")
     
     custom_css = """
